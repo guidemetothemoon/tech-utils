cc38e0ae71c9c19007b5400057a4de53da3877e1
Kristina Devochko <guidemetothemoon@gmail.com>
2022-07-14 13:52:29 +0200

    updated after testing

--- scripts/kubernetes/oauth2-proxy/Deploy-OAuth2-Proxy.ps1
+++ scripts/kubernetes/oauth2-proxy/Deploy-OAuth2-Proxy.ps1
@@ -3,12 +3,49 @@
 
 # ! Pre-requisites: 
 #   - add annotations mentioned in this section of the walkthrough to the Ingress of your application: https://kristhecodingunicorn.com/post/k8s_nginx_oauth/#configure-nginx-ingress-controller
-#   - update configuration and replace placeholders in oauth2-proxy.yaml according to the needs of your application
+#   - update placeholders below according to the needs of your application
 
-# 1. Set up OAuth2 Proxy application in Azure AD
+# 0. Set up initial application values
+$tenantId = [your_azure_ad_tenant_id] # f.ex. "123-456-789"
+$applicationName = [your_application_name] # f.ex. "testapplication" 
+$applicationNamespace = [your_application_namespace] # f.ex. "testns"
+$clientSecretExpirationDate = [your_azure_ad_app_client_secret_end_date] # f.ex. "2022-12-31"
+$appRedirectUri = [uri]"[your_azure_ad_application_redirect_uri]" # f.ex."https://testapp.com/oauth2/callback" 
+$cookieName = [your_oauth2_proxy_cookie_name] # f.ex. "_proxycookie"
+$clusterIssuerName = [your_cert_cluster_issuer_name] # f.ex. "letsencrypt-http01-prod"
 
-# 2. Generate Cookie Secret
+$applicationHostname= $appRedirectUri.Host
+$tempOutputPath = [System.IO.Path]::GetTempPath()
+
+# 1. Create OAuth2 Proxy application in Azure AD
+az login --tenant $tenantId --allow-no-subscriptions
+
+$applicationADApp = az ad app create --display-name $applicationName --sign-in-audience AzureADMyOrg --web-redirect-uris $appRedirectUri | ConvertFrom-Json
+$clientId = $applicationADApp.appId
+$clientSecret = $(az ad app credential reset --id $clientId --append --display-name "$applicationName-client-secret" --end-date $clientSecretExpirationDate --query password --output tsv)
+
+
+# 2. Generate Cookie Secret - in Windows you can install OpenSSL.Light with Chocolatey: choco install OpenSSL.Light
+$cookieSecret = openssl rand -hex 32
 
 # 3. Create Kubernetes Secrets for Client ID, Client Secret and Cookie Secret
 
+kubectl create secret generic client-id --from-literal=oauth2_proxy_client_id=$clientId -n $applicationNamespace
+kubectl create secret generic client-secret --from-literal=oauth2_proxy_client_secret=$clientSecret -n $applicationNamespace
+kubectl create secret generic cookie-secret --from-literal=oauth2_proxy_cookie_secret=$cookieSecret -n $applicationNamespace
+
+# 4. Replace placeholders in OAuth2 Proxy deployment template with provided application values
+(Get-Content "$PSScriptRoot/oauth2-proxy.yaml") `
+|  ForEach-Object `
+	{ `
+		$_  -replace "\[ApplicationName\]", $applicationName `
+			-replace "\[ApplicationNamespace\]", $applicationNamespace `
+			-replace "\[TenantId\]", $tenantId `
+            -replace "\[CookieName\]", $cookieName `
+			-replace "\[CertClusterIssuerName\]", $clusterIssuerName `
+			-replace "\[ApplicationHostname\]", $applicationHostname `
+	} `
+| Set-Content "$tempOutputPath/oauth2-proxy.yaml" -Encoding UTF8
+
 # 4. Install OAuth2 Proxy from the oauth2-proxy.yaml template
+kubectl apply -f "$tempOutputPath/oauth2-proxy.yaml"
