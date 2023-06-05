e61d1daefdabb46db8246ef588ac51caf54f41b1
Kristina Devochko <guidemetothemoon@gmail.com>
2023-06-05 22:45:14 +0200

    Add initial script to add IIS app pool users to local group

---
+++ scripts/azure-monitor/Add-AppPools-To-Group.ps1
@@ -0,0 +1,32 @@
+# This script is related to following blog post on resolving issues with collecting performance
+# counters for Application Insights from ASP.NET applications: [TODO]
+# It will retrieve existing IIS application pools, filter out entries if applicable, and add them
+# to the provided Windows local group.
+
+$appPools = (Get-IISAppPool).Name
+
+# If you want to have some advanced filtering to not add all existing application pools to a group, you can comment out and modify the line below.
+# Here you can use regex expressions to filter out app pools that you're not interested in.
+
+# $appPools=Get-IISAppPool | Where-Object {$_.Name -like 'test*'} | Select -ExpandProperty Name
+
+$formattedAppPools = New-Object System.Collections.ArrayList
+$groupName = "Performance Monitor Users"
+$group = [ADSI]"WinNT://$Env:ComputerName/$groupName,group"
+
+foreach($ap in $appPools)
+{
+    $formattedAP = "IIS APPPOOL\$ap"
+    $formattedAppPools.Add($formattedAP) > $null
+}
+
+foreach($appPool in $formattedAppPools)
+{
+    Write-Output "Adding $appPool to $groupName"
+    
+    # Below block is re-used from https://stackoverflow.com/a/25279322
+    $ntAccount = New-Object System.Security.Principal.NTAccount($appPool)
+    $strSID = $ntAccount.Translate([System.Security.Principal.SecurityIdentifier])
+    $user = [ADSI]"WinNT://$strSID"
+    $group.Add($user.Path)
+}
